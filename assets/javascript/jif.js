a2f4831466d7b9186a12596f712d2339bc6f4a26
Brandon <blshort43@gmail.com>
2016-08-29 23:52:40 -0500

    initial HTML & JS

---
+++ assets/javascript/jif.js
@@ -0,0 +1,3 @@
+var topicArray = ["Jifs", "Gifs", "Deal With It", "Can't Handle My Swag", "Falling", 
+	"Party Hard", "Super Mario", "LOL", "Mashup", "Timelapse", "Slapping", "Slow Mo", 
+	"Weird", "8 Bit", "MacGyver"]
