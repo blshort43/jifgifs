8ed487e95f38ec057afb62a106891e52840d0e79
Brandon <blshort43@gmail.com>
2016-09-01 23:48:25 -0500

    submit button only adds new buttons when text field has input

--- assets/javascript/jif.js
+++ assets/javascript/jif.js
@@ -1,77 +1,82 @@
-var topicArray = ["Jif", "Gif", "Cookies", "Deal With It", "Can't Handle My Swag", "Falling", 
-	"Party Hard", "Super Mario", "LOL", "Mashup", "Timelapse", "Slapping", "Slow Mo", 
-	"Weird", "8 Bit", "MacGyver", "MacGruber", "Carlton Bling"];
+var topicArray = ["Jif", "Gif", "Cookies", "Deal With It", "Can't Handle My Swag", "Falling",
+    "Party Hard", "Super Mario", "LOL", "Mashup", "Timelapse", "Slapping", "Slow Mo",
+    "Weird", "8 Bit", "MacGyver", "MacGruber", "Carlton Bling"
+];
 
 
-window.onload = function(){
-	$("#buttonContainer, .row2").hide();
+window.onload = function() {
+    $("#buttonContainer, .row2").hide();
 
 };
 
-$(".image-responsive").click(function(){
-	$("#buttonContainer, .row2").show();
-	$("#title").remove();
+$(".btn-primary").click(function() {
+    $("#buttonContainer, .row2").show();
+    $("#title").remove();
 });
 
-	function displayGifs(){
+function displayGifs() {
 
-	$("#gifView").empty();
+    $("#gifView").empty();
 
-	var topic = $(this).attr("name")
-	console.log(topic);
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
+    var topic = $(this).attr("name")
+    console.log(topic);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
 
-	$.ajax({url: queryURL, method: 'GET'})
-	 	.done(function(response){
-	 		console.log(response);
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function(response) {
+            console.log(response);
 
-        	var results = response.data;
+            var results = response.data;
 
-        	for(var i=0; i < results.length; i++){
-	 		//gif container
-	 		var gifDiv = $("<div>");
-	 		gifDiv.addClass("gifDiv");
+            for (var i = 0; i < results.length; i++) {
+                //gif container
+                var gifDiv = $("<div>");
+                gifDiv.addClass("gifDiv");
 
-	 		//rating variable
-	 		var rating = response.rating;
+                //rating variable
+                var rating = response.rating;
 
-	 		//rating <p>
-	 		var ratingP = $("<p>").text("Rating: " + results[i].rating);
+                //rating <p>
+                var ratingP = $("<p>").text("Rating: " + results[i].rating);
 
-	 		//display rating
-	 		gifDiv.append(ratingP);	
+                //display rating
+                gifDiv.append(ratingP);
 
-	 		//display gif
-	 		var gifImage = $("<img>").attr('src', results[i].images.fixed_height.url);
+                //display gif
+                var gifImage = $("<img>").attr('src', results[i].images.fixed_height.url);
 
-	 		gifDiv.append(gifImage);
+                gifDiv.append(gifImage);
 
-	 		$('#gifView').append(gifDiv);
-	 		};
-	 	});
-	};
-
-	//create gif buttons
-	function createButtons(){
-	for (var i=0; i<topicArray.length; i++){
-		var button=$("<button>");
-		button.addClass("topicName");
-		button.attr("name", topicArray[i]);
-		button.text(topicArray[i]);
-		$("#buttonContainer").append(button);
-	};
+                $('#gifView').append(gifDiv);
+            };
+        });
 };
 
-$(".submit").click(function(){
-	$("#buttonContainer").empty();
-	var newGif = $(".jifInput").val().trim();
-	topicArray.push(newGif);
-	createButtons();
-	return false;
-	console.log(topicArray);
+//create gif buttons
+function createButtons() {
+    for (var i = 0; i < topicArray.length; i++) {
+        var button = $("<button>");
+        button.addClass("topicName");
+        button.attr("name", topicArray[i]);
+        button.text(topicArray[i]);
+        $("#buttonContainer").append(button);
+    };
+};
+
+$(".submit").click(function() {
+    if (!$('.jifInput').val()) {
+
+    } else {
+        $("#buttonContainer").empty();
+        var newGif = $(".jifInput").val().trim();
+        topicArray.push(newGif);
+        createButtons();
+        $(".jifInput").val("");
+        return false;
+    }
 });
+
 
 $(document).on("click", ".topicName", displayGifs)
 
 createButtons();
-
